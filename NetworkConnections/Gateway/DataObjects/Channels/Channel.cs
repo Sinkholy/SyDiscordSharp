99e776b0eec9186c3b24c8e2f37ddc6d713cc946
Александр Казанин <Sinkholy@gmail.com>
2020-07-05 16:35:06 +0300

    Basic class and interface for voice\text channels

---
+++ NetworkConnections/Gateway/DataObjects/Channels/Channel.cs
@@ -0,0 +1,50 @@
+﻿using Gateway.DataObjects.Channels.Text;
+using Gateway.DataObjects.Guilds;
+using Gateway.DataObjects.Users;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Xml.XPath;
+
+namespace Gateway.DataObjects.Channels
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    internal abstract class Channel : IChannel
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Identifier { get; private set; }
+        public ChannelType Type { get; private set; }
+
+        internal static ChannelType GetChannelType(string type) 
+            => (ChannelType) Enum.Parse(typeof(ChannelType), type);
+
+        #region Ctor's
+        internal Channel(string id, ChannelType type)
+        {
+            Identifier = id;
+            Type = type;
+        }
+        internal Channel(ChannelType type) 
+        {
+            Type = type;
+        }
+        #endregion
+    }
+    public enum ChannelType : byte
+    {
+        GuildText = 0,
+        DirectMessage = 1,
+        GuildVoice = 2,
+        GroupDirectMessage = 3,
+        GuildCategory = 4,
+        GuildNews = 5,
+        GuildStore = 6
+    }
+}
---
+++ NetworkConnections/Gateway/DataObjects/Channels/IChannel.cs
@@ -0,0 +1,67 @@
+﻿using Gateway.DataObjects.Channels.Text;
+using Gateway.DataObjects.Users;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gateway.DataObjects.Channels
+{
+    [JsonConverter(typeof(IChannelConverter))]
+    public interface IChannel
+    {
+        string Identifier { get; }
+        ChannelType Type { get; }
+    }
+
+    internal class IChannelConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType) => objectType == typeof(IChannel);
+        public override bool CanRead => true;
+        public override bool CanWrite => false;
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            JObject json = JObject.Load(reader);
+            string jsonString = json.ToString();
+            ChannelType type = Channel.GetChannelType(json["type"].ToString());
+            IChannel result;
+            switch (type)
+            {
+                case ChannelType.DirectMessage:
+                    result = new DMTextChannel(type);
+                    break;
+                case ChannelType.GroupDirectMessage:
+                    result = new GroupDMTextChannel(type);
+                    break;
+                case ChannelType.GuildCategory:
+                    result = new ChannelCategory(type);
+                    break;
+                case ChannelType.GuildNews:
+                    result = new GuildNewsChannel(type);
+                    break;
+                case ChannelType.GuildStore:
+                    result = new GuildStoreChannel(type);
+                    break;
+                case ChannelType.GuildText:
+                    result = new GuildTextChannel(type);
+                    break;
+                case ChannelType.GuildVoice:
+                    result = new GuildVoiceChannel(type);
+                    break;
+                default:
+                    throw new Exception("Unknown channel type"); //TODO : адекватное исключение
+            }
+            JsonConvert.PopulateObject(jsonString, result);
+            return result;
+        }
+        /// <summary>
+        /// Not implemented
+        /// </summary>
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer) 
+            => throw new NotImplementedException();
+    }
+}
