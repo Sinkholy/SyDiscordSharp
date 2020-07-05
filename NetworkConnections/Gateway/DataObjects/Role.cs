0b3b56713c96aea3799e22a20c4a3d3760379c5c
Александр Казанин <Sinkholy@gmail.com>
2020-07-05 16:57:15 +0300

    Role class

---
+++ NetworkConnections/Gateway/DataObjects/Role.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gateway.DataObjects
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    public class Role
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Identifier;
+        [JsonProperty(PropertyName = "name")]
+        public string Name;
+        [JsonProperty(PropertyName = "color")]
+        public int Color; //Hexadecimal
+        [JsonProperty(PropertyName = "position")]
+        public int Position;
+        [JsonProperty(PropertyName = "permissions")]
+        public int Permissions;
+        [JsonProperty(PropertyName = "hoist")]
+        public bool Hoist;
+        [JsonProperty(PropertyName = "managed")]
+        public bool Managed;
+        [JsonProperty(PropertyName = "mentionable")]
+        public bool Mentionable;
+    }
+}
