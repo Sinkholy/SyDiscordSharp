3aac6665a1eaf0aa39bba7d697f81f13978355ff
Александр Казанин <Sinkholy@gmail.com>
2020-07-05 16:38:46 +0300

    Interface for guild channel, besides from guild here are also dm\group dm channels

---
+++ NetworkConnections/Gateway/DataObjects/Channels/IGuildChannel.cs
@@ -0,0 +1,16 @@
+﻿using Gateway.DataObjects.Guilds;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gateway.DataObjects.Channels
+{
+    interface IGuildChannel : IChannel
+    {
+        string GuildIdentifier { get; }
+
+        void UpdateChannelGuildId(IGuild guild);
+    }
+}
