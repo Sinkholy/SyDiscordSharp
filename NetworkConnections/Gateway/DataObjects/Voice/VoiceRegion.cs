06ce5e06ad94a1bfed492e7d7ec56874d37159eb
Александр Казанин <Sinkholy@gmail.com>
2020-07-05 17:20:47 +0300

    Classes for voice region and state of discord guild

---
+++ NetworkConnections/Gateway/DataObjects/Voice/VoiceRegion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gateway.DataObjects.Voice
+{
+    internal class VoiceRegion
+    {
+        string Identifier,
+               Name;
+        bool Vip,
+             Optimal,
+             Deprecated,
+             Custom;
+    }
+}
---
+++ NetworkConnections/Gateway/DataObjects/Voice/VoiceState.cs
@@ -0,0 +1,24 @@
+﻿using Gateway.DataObjects.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gateway.DataObjects.Voice
+{
+    internal class VoiceState
+    {
+        string GuildIdentifier,
+               ChannelIdentifier,
+               UserIdentifier,
+               SessionIdentifier;
+        GuildUser GuildUser;
+        bool Deafened,
+             Muted,
+             SelfDeafened,
+             SelfMuted,
+             SelfStream,
+             Suppress;
+    }
+}
