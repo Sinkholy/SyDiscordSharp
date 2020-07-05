8f59964d63a9d9ee6bf6883276d2132ffbe1b320
Александр Казанин <Sinkholy@gmail.com>
2020-07-05 16:31:58 +0300

    Classes for emojis

---
+++ NetworkConnections/Gateway/DataObjects/Emojis/Emoji.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gateway.DataObjects.Emojis
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    public class Emoji : IEmoji
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Identifier { get; private set; }
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; private set; }
+        [JsonProperty(PropertyName = "animated")]
+        public bool Animated { get; private set; }
+    }
+}
---
+++ NetworkConnections/Gateway/DataObjects/Emojis/GuildEmoji.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gateway.DataObjects.Emojis
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    public class GuildEmoji : Emoji
+    {
+        [JsonProperty(PropertyName = "roles")]
+        public Role[] Roles;
+        [JsonProperty(PropertyName = "require_colons")]
+        public bool RequireColons;
+        [JsonProperty(PropertyName = "managed")]
+        public bool Managed;
+        [JsonProperty(PropertyName = "available")]
+        public bool Available;
+    }
+}
