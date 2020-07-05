9df697e3bf310fa0235740e1f68de7e9f2d176b6
Александр Казанин <Sinkholy@gmail.com>
2020-07-05 17:04:39 +0300

    Adding logic to gateway client class

--- NetworkConnections/Gateway/DiscordGatewayClient.cs
+++ NetworkConnections/Gateway/DiscordGatewayClient.cs
@@ -1,28 +1,72 @@
-﻿using Gateway.Enums;
-using Gateway.PayloadObjects;
+﻿using Gateway.DataObjects;
+using Gateway.DataObjects.Guilds;
+using Gateway.DataObjects.Users;
+using Gateway.Enums;
+using Gateway.Payload.DataObjects;
+using Gateway.Payload.EventObjects;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Net.WebSockets;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace Gateway
 {
-    public class DiscordGatewayClient
+    public class DiscordGatewayClient //TODO : sharding
     {
-        private readonly string botToken = "NTU5MDkwMTUzOTM1NjAxNjY1.Xqx1NQ.egtltET15L0bDfA89oGOr2MVXr0";
-        private ClientWebSocket clientWebSocket;
+        private readonly ConcurrentQueue<GatewayPayload> payloadsReceived;
+        private readonly string botToken = "NTU5MDkwMTUzOTM1NjAxNjY1.XtKPog.7epgH4xS8QxLqGgiGyBLCladnyI"; //TODO : перенести токен в конфиг и объеденить методы с API
         private readonly JsonSerializer jsonSerializer;
+        private Gateway gateway;
+        private readonly PayloadHandler payloadHandler;
+        private readonly EventHandler eventHandler;
+        private ClientWebSocket clientWebSocket;
+        private short identifyLimit; // TODO : метод обновлящий значение при отправке новой идентификации
+                                    // и изначальное записывание значения полученое при первичном запросе к HTTP API
+        private DateTime readyReceived;
+        private Dictionary<string, IGuild> guilds;
+        private Dictionary<string, List<string>> userGuilds;
+
+        internal IGuild[] Guilds => guilds.Values.ToArray();
+        internal User BotUser;
+        internal Uri GatewayUri { get; private set; }//RO?
+        public TimeSpan Uptime => DateTime.Now - readyReceived;
+
+        #region Event handlers
+        private void OnConnection() => Console.WriteLine(1);//TODO : do smth
+        private void OnReady(IEvent eventData)
+        {
+            Ready ready = eventData as Ready;
+            BotUser = ready.User;
+            foreach(var guild in ready.Guilds) 
+                guilds.Add(guild.Identifier, guild as IGuild);
+            readyReceived = DateTime.Now;
+        }
+        private void OnGuildCreate(IEvent eventData)
+        {
+            IGuild guild = eventData as IGuild;
+            if (guilds.ContainsKey(guild.Identifier)) 
+                guilds.Add(guild.Identifier, guild);
+            else
+                guilds[guild.Identifier].UpdateGuild(guild);
+        }
+        #endregion
         private string SerializeJson(object toSerialize)
         {
             var sb = new StringBuilder(256); //TODO: подумать над размером буффера 
@@ -44,49 +88,61 @@ namespace Gateway
         }
         private TObject DeserializeJson<TObject>(string value)
         {
-            using (TextReader textReader = new StringReader(value))
-            {
-                using (JsonReader jsonReader = new JsonTextReader(textReader))
-                {
-                    return (TObject)jsonSerializer.Deserialize(jsonReader);
-                }
-            }
+            return JsonConvert.DeserializeObject<TObject>(value); // TAI: он тут вообще нужен?
         }
-        public Uri GatewayUri { get; private set; }//RO?
-        public async Task StartAsync() //Мне не очень хочется передавать Uri параметром, мб подтянуть из конфига или что-нибудь другое хз
-        {
-            await ConnectToGateway();
-            await clientWebSocket.ReceiveAsync(new ArraySegment<byte>(new byte[4096]), CancellationToken.None);
-            Console.WriteLine(this.clientWebSocket.State.ToString());
-            await TcpSocketTest();
-        }
-        private async Task ConnectToGateway()
-        {
-            await clientWebSocket.ConnectAsync(GatewayUri, CancellationToken.None);
-        }
-        private async Task TcpSocketTest()
-        {
-            IdentityProperties properties = new IdentityProperties("SinkholesImpl", "SinkholesDevice");
-            Identity identityObj = new Identity(botToken, properties);
 
-            GatewayPayload payload = new GatewayPayload(Opcode.Identity, identityObj);          
-
-            await clientWebSocket.SendAsync(new ArraySegment<byte>(Encoding.Default.GetBytes(SerializeJson(payload))), WebSocketMessageType.Text, true, CancellationToken.None);
-            Console.WriteLine("Sended");
-            byte[] recieveBuffer = new byte[1024 * 4];
-            await clientWebSocket.ReceiveAsync(new ArraySegment<byte>(recieveBuffer), CancellationToken.None);
-            string str = Encoding.Default.GetString(recieveBuffer);
-            Console.WriteLine("Recieved");
-            Console.ReadLine();
-        }
-        private async Task SendAuthorization()
+        private async Task AuthorizeAsync()
         {
-
+            IdentifyProperties properties = new IdentifyProperties("SinkholesImpl", "SinkholesDevice");
+            Identify identityObj = new Identify(botToken, properties, IdentifyIntents.None);
+            GatewayPayload payload = new GatewayPayload(Opcode.Identify, identityObj);
+            await gateway.SendAsync(clientWebSocket, payload, WebSocketMessageType.Text, CancellationToken.None);
         }
-        public DiscordGatewayClient(Uri gatewayUri)
+        private void AddToQueue(string eventData)
+        {
+            GatewayPayload payload = DeserializeJson<GatewayPayload>(eventData);
+            payloadsReceived.Enqueue(payload);
+        }
+        public async Task StartAsync(Uri gatewayUri) //TAI : подписать этот метод на некое событие в HHTP-клиенте сигнализирующее о получении /gateway ответа 
         {
             GatewayUri = gatewayUri;
+            gateway = new Gateway(clientWebSocket, GatewayUri, botToken);
+            #region Event's handler's binding
+            payloadHandler.Connected += gateway.OnConnection;
+            payloadHandler.HeartbeatACK += gateway.OnHeartbeatAck;
+            payloadHandler.NewSequenceReceived += gateway.OnSequenceReceived;
+            payloadHandler.NewEvent += eventHandler.OnNewEventCreated;
+            eventHandler.GuildCreate += OnGuildCreate;
+            #region ready
+            eventHandler.Ready += OnReady;
+            eventHandler.Ready += gateway.OnReady;
+            #endregion
+
+            gateway.NewPayload += AddToQueue;
+            #endregion
+            await gateway.ConnectToGatewayAsync();
+            await AuthorizeAsync();
+        }
+        public IGuild[] GetUserGuilds(string userId)
+        {
+            List<string> guildsIdentifiers = userGuilds[userId];
+            List<IGuild> guilds = new List<IGuild>();
+            foreach(var guild in guildsIdentifiers)
+            {
+                guilds.Add(this.guilds[guild]);
+            }
+            return guilds.ToArray();
+        }
+        public IGuild[] GetUserGuilds(IUser user) => GetUserGuilds(user.Identifier);
+        public DiscordGatewayClient() 
+        {
+            guilds = new Dictionary<string, IGuild>();
+            payloadsReceived = new ConcurrentQueue<GatewayPayload>();
             clientWebSocket = new ClientWebSocket();
+            jsonSerializer = new JsonSerializer();
+            payloadHandler = new PayloadHandler(payloadsReceived);
+            eventHandler = new EventHandler();
         }
     }
 }
+
