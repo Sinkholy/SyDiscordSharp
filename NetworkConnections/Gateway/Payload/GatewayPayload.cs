b0965f6ec572baa0b764b01b61073def4f189091
Александр Казанин <Sinkholy@gmail.com>
2020-07-05 17:09:21 +0300

    Added custom JsonConverter for IGateway which I needed to deserialize Discord gateway objects to IGateway objects

--- NetworkConnections/Gateway/Payload/GatewayPayload.cs
+++ NetworkConnections/Gateway/Payload/GatewayPayload.cs
@@ -1,16 +1,21 @@
 ﻿using Gateway.Enums;
-using Gateway.PayloadObjects;
+using Gateway.Payload.DataObjects;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Gateway
 {
     [JsonObject(MemberSerialization.OptIn)]
-    class GatewayPayload
+    [JsonConverter(typeof(IGatewayPayloadConverter))]
+    public class GatewayPayload
     {
         [JsonProperty(propertyName: "op", Order = 2)]
         public Opcode Opcode;
@@ -20,13 +25,76 @@ namespace Gateway
         public string Sequence = null;
         [JsonProperty(propertyName: "t", Order = 0)]
         public string EventName = null;
-
         public GatewayPayload(Opcode opcode, IGatewayDataObject data, string eventName = null, string sequence = null)
         {
             this.Opcode = opcode;
             this.Data = data;
-            this.Sequence = sequence;
-            this.EventName = eventName;
+            this.Sequence = sequence == "" ? null : sequence;
+            this.EventName = eventName == "" ? null : eventName;
+        }
+
+        private class IGatewayPayloadConverter : JsonConverter
+        {
+            public override bool CanConvert(Type objectType) => objectType == typeof(IGatewayDataObject);
+            public override bool CanRead => true;
+            public override bool CanWrite => false;
+            public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+            {
+                JObject json = JObject.Load(reader);
+                string dataJson = json["d"].ToString(),
+                       opcodeJson = json["op"].ToString(),
+                       sequenceJson = json["s"].ToString(),
+                       eventNameJson = json["t"].ToString(),
+                       dataTypeFullName;
+                Opcode opcode = ParseOpcode(opcodeJson);
+                dataTypeFullName = $"{objectType.Namespace}.Payload.DataObjects.{opcode}";
+                Type dataType = objectType.Assembly.GetType(dataTypeFullName);
+                IGatewayDataObject dataObject = JsonConvert.DeserializeObject(dataJson, dataType) as IGatewayDataObject;
+
+                return new GatewayPayload(opcode, dataObject, eventNameJson, sequenceJson);
+            }
+            private Opcode ParseOpcode(string opcodeJson)
+            {
+                byte opcode;
+                try
+                {
+                    opcode = byte.Parse(opcodeJson);
+                }
+                catch
+                {
+                    throw new Exception("cannot parse opcode"); // TODO : исключение
+                }
+
+                byte[] enumValues = (byte[])Enum.GetValues(typeof(Opcode));
+                bool opcodeFounded = false;
+                for (byte i = 0; i < enumValues.Length; i++)
+                {
+                    byte current = enumValues[i];
+                    if (opcode == current)
+                    {
+                        opcodeFounded = true;
+                        opcode = current;
+                        break;
+                    }
+                    else
+                        continue;
+                }
+                if (opcodeFounded)
+                {
+                    return (Opcode)opcode;
+                }
+                else
+                {
+                    throw new Exception("unknown opcode"); //TODO : исключение
+                }
+            }
+            /// <summary>
+            /// Not implemented
+            /// </summary>
+            public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+            {
+                throw new NotImplementedException();
+            }
         }
     }
 }
