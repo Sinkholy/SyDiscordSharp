75661947361c579d6d66b2f3f569a1e4f9ee20ee
Александр Казанин <Sinkholy@gmail.com>
2020-07-05 17:23:14 +0300

    Minor changes to hello event object

--- NetworkConnections/Gateway/Payload/DataObjects/Hello.cs
+++ NetworkConnections/Gateway/Payload/DataObjects/Hello.cs
@@ -1,16 +1,25 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Gateway.PayloadObjects
+namespace Gateway.Payload.DataObjects
 {
     [JsonObject(MemberSerialization.OptIn)]
-    class Hello
+    class Hello : IGatewayDataObject
     {
-        [JsonProperty(PropertyName = "heartbet_interval", Order = 0)]
+        [JsonProperty(PropertyName = "heartbeat_interval", Order = 0)]
         public TimeSpan HeartbeatInterval;
+        [JsonProperty(PropertyName = "_trace", Order = 1)]
+        public object Trace;
+        [JsonConstructor]
+        public Hello(int heartbeat_interval, object _trace)
+        {
+            HeartbeatInterval = TimeSpan.FromMilliseconds(heartbeat_interval);
+            Trace = _trace;
+        }
     }
 }
