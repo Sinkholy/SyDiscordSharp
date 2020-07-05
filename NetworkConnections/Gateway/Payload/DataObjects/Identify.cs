61ce687db9f901676fe07acae68aaa14571264a0
Александр Казанин <Sinkholy@gmail.com>
2020-07-05 17:34:47 +0300

    Fixed naming and add some logic to identify

--- NetworkConnections/Gateway/Payload/DataObjects/Identify.cs
+++ NetworkConnections/Gateway/Payload/DataObjects/Identify.cs
@@ -6,10 +6,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Gateway.PayloadObjects
+namespace Gateway.Payload.DataObjects
 {
     [JsonObject(MemberSerialization.OptIn)]
-    class Identity : IGatewayDataObject
+    internal class Identify : IGatewayDataObject
     {
         [JsonProperty(propertyName: "token", Order = 0)]
         public string Token;
@@ -24,21 +24,24 @@ namespace Gateway.PayloadObjects
         //[JsonProperty(propertyName: "presence", Order = 6)]
         //public Presences Presences;
         [JsonProperty(propertyName: "properties", Order = 1)]
-        public IdentityProperties Properties;
-
-        public Identity(string token,
-                        IdentityProperties properties,
+        public IdentifyProperties Properties;
+        [JsonProperty(propertyName: "intents", Order = 555)] //TODO : order, они вообще нужны?
+        public string Intents;
+        public Identify(string token,
+                        IdentifyProperties properties,
+                        IdentifyIntents intents = null,
                         bool compress = false,
                         int largeThreshold = 50,
                         bool guildSubscriptions = true)
         {
             this.Properties = properties;
             this.Token = token;
+            this.Intents = intents is null ? null : intents.Intents;
             //this.Compress = compress;
             //this.LargeThreshold = largeThreshold;
             //this.GuildSubscriptions = guildSubscriptions;
         }
-        //public Identity(string token,
+        //public Identify(string token,
         //                Presences presences,
         //                IdentityProperties properties,
         //                bool compress = false, int largeThreshold = 50,
@@ -49,7 +52,7 @@ namespace Gateway.PayloadObjects
         //}
     }
     [JsonObject(MemberSerialization.OptIn)]
-    public class IdentityProperties
+    internal class IdentifyProperties
     {
         [JsonProperty(propertyName: "$os", Order = 0)]
         public string Os = Environment.OSVersion.ToString();
@@ -57,10 +60,26 @@ namespace Gateway.PayloadObjects
         public string Browser;
         [JsonProperty(propertyName: "$device", Order = 2)]
         public string Device;
-        public IdentityProperties(string browser, string device)
+        public IdentifyProperties(string browser, string device)
         {
             this.Browser = browser;
             this.Device = device;
+        }
+    }
+    internal class IdentifyIntents
+    {
+        internal string Intents { get; private set; }
+        internal static IdentifyIntents None = null,
+                                        Default = new IdentifyIntents(1 << 0 | 1 << 2 | 1 << 3 |
+                                                                      1 << 4 | 1 << 5 | 1 << 6 |
+                                                                      1 << 7 | 1 << 9 | 1 << 10 |
+                                                                      1 << 11 | 1 << 12 | 1 << 13 |
+                                                                      1 << 14),
+                                        Custom = new IdentifyIntents(GetCustomValue());
+        private IdentifyIntents(int intents) => Intents = intents.ToString();
+        private static int GetCustomValue() //TODO :чтение из конфига очвдно
+        {
+            return 0; 
         }
     }
 }
