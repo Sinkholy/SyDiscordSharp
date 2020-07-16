9f35c28523e6e8be194bd3869969b0b360e1c77e
Александр Казанин <Sinkholy@gmail.com>
2020-07-16 14:09:44 +0300

    Ban-class

---
+++ NetworkConnections/Gateway/DataObjects/Ban.cs
@@ -0,0 +1,29 @@
+﻿using Gateway.DataObjects.Guilds;
+using Gateway.DataObjects.Users;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gateway.DataObjects
+{
+    internal class Ban
+    {
+        internal IGuild Guild { get; private set; }
+        [JsonProperty(PropertyName = "user")]
+        internal User User { get; private set; }
+
+        [JsonProperty(PropertyName = "guild_id")]
+        private string guildIdentifier;       
+
+        [OnDeserialized]
+        private void CompleteDeserialization(StreamingContext context)
+        {
+            Guild = DiscordGatewayClient.TryToGetGuild(guildIdentifier);
+        }
+    }
+}
