7a2892174e025fab71245c2d229a4e37255477a7
Александр Казанин <Sinkholy@gmail.com>
2020-07-16 13:51:20 +0300

    Added classes representing discord invite and IInvite interface for out-of-project use

---
+++ NetworkConnections/Gateway/DataObjects/Invite/IInvite.cs
@@ -0,0 +1,17 @@
+﻿using Gateway.DataObjects.Channels;
+using Gateway.DataObjects.Guilds;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gateway.DataObjects.Invite
+{
+    internal interface IInvite
+    {
+        string Code { get; }
+        IGuild Guild { get; }
+        IChannel Channel { get; }
+    }
+}
---
+++ NetworkConnections/Gateway/DataObjects/Invite/Invitebase.cs
@@ -0,0 +1,36 @@
+﻿using Gateway.DataObjects.Channels;
+using Gateway.DataObjects.Guilds;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gateway.DataObjects.Invite
+{
+    internal class InviteBase : IInvite
+    {
+        [JsonProperty(PropertyName = "code")]
+        public string Code { get; private protected set; }
+        public IGuild Guild { get; private protected set; }
+        public IChannel Channel { get; private protected set; }
+
+        [JsonProperty(PropertyName = "guild_id")]
+        internal protected string GuildIdentifier { get; private set; }
+        [JsonProperty(PropertyName = "channel_id")]
+        internal protected string ChannelIdentifier { get; private set; }
+
+        [OnDeserialized]
+        private void CompleteDeserialization(StreamingContext context)
+        {
+            Guild targetGuild = DiscordGatewayClient.TryToGetGuild(GuildIdentifier) as Guild;
+            IChannel targetChannel = null;            
+            if (targetGuild != null)
+                targetChannel = targetGuild.TryToGetChannel(ChannelIdentifier);
+            Guild = targetGuild;
+            Channel = targetChannel;
+        }
+    }
+}
