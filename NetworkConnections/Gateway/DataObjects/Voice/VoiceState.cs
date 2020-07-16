06bf1e7198b9d2583d0ecbc8fd5698bfc410e80f
Александр Казанин <Sinkholy@gmail.com>
2020-07-16 14:25:51 +0300

    Implemented VoiceState object. At this point it is used in Guild obj.

--- NetworkConnections/Gateway/DataObjects/Voice/VoiceState.cs
+++ NetworkConnections/Gateway/DataObjects/Voice/VoiceState.cs
@@ -1,7 +1,11 @@
-﻿using Gateway.DataObjects.Users;
+﻿using Gateway.DataObjects.Channels;
+using Gateway.DataObjects.Guilds;
+using Gateway.DataObjects.Users;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,16 +13,50 @@ namespace Gateway.DataObjects.Voice
 {
     internal class VoiceState
     {
-        string GuildIdentifier,
-               ChannelIdentifier,
-               UserIdentifier,
-               SessionIdentifier;
-        GuildUser GuildUser;
-        bool Deafened,
-             Muted,
-             SelfDeafened,
-             SelfMuted,
-             SelfStream,
-             Suppress;
+        internal IUser User { get; private set; }
+        [JsonProperty(PropertyName = "user_id")]
+        internal string UserIdentifier { get; private set; }
+        internal IChannel Channel { get; private set; }
+        [JsonProperty(PropertyName = "channel_id")]
+        internal string ChannelIdentifier { get; private set; }
+        internal IGuild Guild { get; private set; }
+        [JsonProperty(PropertyName = "guild_id")]
+        internal string GuildIdentifier { get; private set; }
+        [JsonProperty(PropertyName = "session_id")]
+        internal string SessionIdentifier { get; private set; }
+        [JsonProperty(PropertyName = "deaf")]
+        internal bool Deafened { get; private set; }
+        [JsonProperty(PropertyName = "mute")]
+        internal bool Muted { get; private set; }
+        [JsonProperty(PropertyName = "self_deaf")]
+        internal bool SelfDeafened { get; private set; }
+        [JsonProperty(PropertyName = "self_mute")]
+        internal bool SelfMuted { get; private set; }
+        [JsonProperty(PropertyName = "self_stream")]
+        internal bool SelfStream { get; private set; }
+        [JsonProperty(PropertyName = "suppress")]
+        internal bool Suppressed { get; private set; }
+
+        [JsonProperty(PropertyName = "member")]
+        private GuildUser guildUser;
+        [OnDeserialized]
+        private void CompleteDeserialization(StreamingContext context)
+        {
+            IGuild targetGuild = null;
+            if (GuildIdentifier != null)
+                targetGuild = DiscordGatewayClient.TryToGetGuild(GuildIdentifier);
+            IUser targetUser = null;
+            IChannel targetChannel = null;
+            if(targetGuild is Guild guild)
+            {
+                targetUser = guild.TryToGetUser(UserIdentifier);
+                targetChannel = guild.TryToGetChannel(ChannelIdentifier);
+            }
+            if (targetUser == null && guildUser != null)
+                targetUser = guildUser as IUser;
+            Guild = targetGuild;
+            User = targetUser;
+            Channel = targetChannel;
+        }
     }
 }
