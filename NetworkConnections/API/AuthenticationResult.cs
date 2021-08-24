280e1e3cf2ea2e2db083e6f35039b37c08b8e936
Sinkhole <Sinkholy@gmail.com>
2021-08-24 20:22:00 +0300

    Added AuthenticationResult to hide unnecessary details about http communication

---
+++ NetworkConnections/API/AuthenticationResult.cs
@@ -0,0 +1,14 @@
+﻿namespace Http
+{
+	public class AuthenticationResult
+	{
+		internal AuthenticationResult(bool isSuccessfull, string faultReason)
+		{
+			IsSuccessfull = isSuccessfull;
+			FaultReason = faultReason;
+		}
+
+		public bool IsSuccessfull { get; private set; }
+		public string FaultReason { get; private set; }
+	}
+}
