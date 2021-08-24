e912e195c74b88c02e369ddcfa81a74b0dc8e90f
Sinkhole <Sinkholy@gmail.com>
2021-08-24 23:25:10 +0300

    Added public ctor

--- NetworkConnections/API/Connection/Header.cs
+++ NetworkConnections/API/Connection/Header.cs
@@ -2,6 +2,12 @@
 {
 	public class Header
 	{
+		public Header(string scheme, string parameter)
+		{
+			Scheme = scheme;
+			Parameter = parameter;
+		}
+
 		internal string Scheme { get; private set; }
 		internal string Parameter { get; private set; }
 	}
