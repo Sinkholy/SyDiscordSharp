2d7290e90997a1ef1be986535ff871fe63982e70
Sinkhole <Sinkholy@gmail.com>
2021-08-25 21:21:57 +0300

    [Development] Basic implementation of wrapper.

--- SystemNetHttpCommunicationWrapper/SystemNetHttpClientWrapper.cs
+++ SystemNetHttpCommunicationWrapper/SystemNetHttpClientWrapper.cs
@@ -6,95 +6,107 @@ using Http.Connection;
 
 using HttpCommunication.Connection;
 
-using HttpResponseMessage = HttpCommunication.Connection.HttpResponseMessage;
+using OriginalRequestMessage = System.Net.Http.HttpRequestMessage;
+using OriginalResponseMessage = System.Net.Http.HttpResponseMessage;
+using WrappedRequestMessage = HttpCommunication.Connection.HttpRequestMessage;
+using WrappedResponseMessage = HttpCommunication.Connection.HttpResponseMessage;
 
 namespace SystemNetHttpCommunicationWrapper
 {
-	class SystemNetHttpClientWrapper : IHttpConnection
+	public class SystemNetHttpClientWrapper : IHttpConnection
 	{
-		const string PatchHttpMethod = "PATCH";
+		readonly HttpMethod PatchMethod;
 		readonly HttpClient httpClient;
 
 		public SystemNetHttpClientWrapper()
 		{
-			// Инициализировать HttpClient
-			// Инициализировать Timeout стандартным значением
-			// Инициализировать BaseAddress стандартным значением
-			// Инициализировать Headers используя HeadersWrapper класс
+			httpClient = new HttpClient();
+			BaseAdress = default;
+			Headers = new HeadersWrapper(this);
+			PatchMethod = new HttpMethod("PATCH");
 		}
 
-		public Uri BaseAdress { get; set; }
-		public TimeSpan Timeout { get; set; }
+		public Uri BaseAdress
+		{
+			get => httpClient.BaseAddress;
+			set => httpClient.BaseAddress = value;
+		}
+		public TimeSpan Timeout
+		{
+			get => httpClient.Timeout;
+			set => httpClient.Timeout = value;
+		}
 		public IHeaders Headers { get; private set; }
 
-		public async Task<HttpResponseMessage> DeleteAsync(HttpRequestMessage message)
+		public async Task<WrappedResponseMessage> DeleteAsync(WrappedRequestMessage message)
 		{
-			// Отправить запрос и дождаться его выполнения
-			// Преобразовать полученный результат в HttpResponseMessage
-			// Вернуть результат
+			OriginalResponseMessage response = await httpClient.DeleteAsync(message.DestinationEndpoint);
+			WrappedResponseMessage result = ConvertReceivingMessage(response);
+			return result;
 		}
-		public async Task<HttpResponseMessage> GetAsync(HttpRequestMessage message)
+		public async Task<WrappedResponseMessage> GetAsync(WrappedRequestMessage message)
 		{
-			// Отправить запрос и дождаться его выполнения
-			// Преобразовать полученное сообщения под IHttpResponseMessage интерфейс
-			// Вернуть результат
+			OriginalResponseMessage response = await httpClient.GetAsync(message.DestinationEndpoint);
+			WrappedResponseMessage result = ConvertReceivingMessage(response);
+			return result;
 		}
-		public async Task<HttpResponseMessage> PatchAsync(HttpRequestMessage message)
+		public async Task<WrappedResponseMessage> PatchAsync(WrappedRequestMessage message)
 		{
-			// Подготовить HttpMethod в связи с тем, что Patch не входит в стандартный набор
-			// Создать System.Net.Http.HttpRequestMessage
-			// Отправить запрос и дождаться его выполнения
-			// Преобразовать полученный результат в HttpResponseMessage
-			// Вернуть результат
-		}
-		public async Task<HttpResponseMessage> PostAsync(HttpRequestMessage message)
-		{
-			// Преобразовать входной контент в HttpContent
-			// Заполнить поле Uri взяв его из параметра Message
-			// Отправить запрос и дождаться его выполнения
-			// Преобразовать полученный результат в HttpResponseMessage
-			// Вернуть результат
-		}
-		public async Task<HttpResponseMessage> PutAsync(HttpRequestMessage message)
-		{
-			// Преобразовать входной контент в HttpContent
-			// Отправить запрос и дождаться его выполнения
-			// Преобразовать полученный результат в HttpResponseMessage
-			// Вернуть результат
-		}
-		public Header GetAuthorizationHeader()
-		{
-			// Перенаправить на вызов заголовка у хттп клиента
-		}
-		public void SetAuthorizationHeader()
-		{
-			// Перенаправить на вызов заголовка у хттп клиента
-		}
-
-		HttpContent ConvertSendingContent(IHttpContent toConvert)
-		{
-			// Прочитать контент из toConvert в строчном формате
-			// Создать экземпляр типа StringContent 
-			// Подставить в StringContent контент из toConvert
-			// Вернуть результат
-		}
-		HttpResponseMessage ConvertReceivingMessage(HttpResponseMessage toConvert)
-		{
-			// Если в сообщении присутствует контент
-				// Преобразовать полученный контент в IHttpContent (ConvertReceivingContent)
-			// Преобразовать System.Net.HttpStatusCode в HttpStatusCode (ConvertSystemNetHttpStatusCode)
-			// Создать HttpResponseMessage
-			// Вернуть результат
-
-			IHttpContent ConvertReceivingContent(HttpContent toConvert)
+			var requestMessage = new OriginalRequestMessage()
 			{
-				// Преобразовать
-				// Вернуть результат
+				RequestUri = new Uri(message.DestinationEndpoint),
+				Method = PatchMethod,
+				Content = await ConvertSendingContent(message.Content)
+			};
+			OriginalResponseMessage response = await httpClient.SendAsync(requestMessage);
+			WrappedResponseMessage result = ConvertReceivingMessage(response);
+			return result;
+		}
+		public async Task<WrappedResponseMessage> PostAsync(WrappedRequestMessage message)
+		{
+			HttpContent contentToSend = await ConvertSendingContent(message.Content);
+			OriginalResponseMessage response = await httpClient.PostAsync(message.DestinationEndpoint, contentToSend);
+			WrappedResponseMessage result = ConvertReceivingMessage(response);
+			return result;
+		}
+		public async Task<WrappedResponseMessage> PutAsync(WrappedRequestMessage message)
+		{
+			HttpContent contentToSend = await ConvertSendingContent(message.Content);
+			OriginalResponseMessage response = await httpClient.PutAsync(message.DestinationEndpoint, contentToSend);
+			WrappedResponseMessage result = ConvertReceivingMessage(response);
+			return result;
+		}
+
+		async Task<HttpContent> ConvertSendingContent(IHttpContent toConvert)
+		{
+			var stringContent = await toConvert.ReadAsStringAsync();
+			var convertedContent = new StringContent(stringContent);
+			return convertedContent;
+		}
+		WrappedResponseMessage ConvertReceivingMessage(OriginalResponseMessage toConvert)
+		{
+			IHttpContent convertedContent = null;
+			if(MessageContainsContent())
+			{
+				convertedContent = ConvertReceivingContent();
 			}
-			HttpStatusCode ConvertSystemNetHttpStatusCode(System.Net.HttpStatusCode toConvert)
+			HttpStatusCode convertedStatusCode = ConvertSystemNetHttpStatusCode();
+			var result = new WrappedResponseMessage(toConvert.IsSuccessStatusCode, convertedStatusCode, toConvert.ReasonPhrase, convertedContent);
+			return result;
+
+			bool MessageContainsContent()
 			{
-				// Проеобразовать
-				// Вернуть результат
+				return toConvert.Content != null;
+			}
+			IHttpContent ConvertReceivingContent()
+			{
+				return SystemNetHttpMessageContentWrapper.WrapFrom(toConvert.Content);
+			}
+			HttpStatusCode ConvertSystemNetHttpStatusCode() 
+			{
+				// UNDONE: здесь нужен Assert по поводу того, что имеется неизвестный код статуса
+				// UNDONE: Так же нужна обработка исключения преобразования
+				return (HttpStatusCode)toConvert.StatusCode;
 			}
 		}
 
@@ -102,28 +114,20 @@ namespace SystemNetHttpCommunicationWrapper
 		{
 			readonly SystemNetHttpClientWrapper client;
 
-			// Конструктор
+			internal HeadersWrapper(SystemNetHttpClientWrapper client)
+			{
+				this.client = client;
+			}
+
 			public Header AuthorizationHeader
 			{
-				get
-				{
-					// Перенаправить на вызов GetAuthorizeHeader у хттп клиента
-				}
-				set
-				{
-					// Перенаправить на вызов SetAuthorizeHeader у хттп клиента
-				}
+				get => client.Headers.AuthorizationHeader;
+				set => client.Headers.AuthorizationHeader = value;
 			}
 			public Header UserAgentHeader
 			{
-				get
-				{
-					// Перенаправить на вызов GetUserAgent у хттп клиента
-				}
-				set
-				{
-					// Перенаправить на вызов SetUserAgent у хттп клиента
-				}
+				get => client.Headers.UserAgentHeader;
+				set => client.Headers.UserAgentHeader = value;
 			}
 		}
 	}
