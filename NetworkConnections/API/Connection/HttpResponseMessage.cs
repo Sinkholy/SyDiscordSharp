926a9f2d6404eb11d4d560064c7292ad526ab393
Sinkhole <Sinkholy@gmail.com>
2021-08-25 21:29:37 +0300

    [Development] Added ctor.

--- NetworkConnections/API/Connection/HttpResponseMessage.cs
+++ NetworkConnections/API/Connection/HttpResponseMessage.cs
@@ -2,6 +2,14 @@
 {
 	public class HttpResponseMessage
 	{
+		public HttpResponseMessage(bool isSuccessStatusCode, HttpStatusCode statusCode, string reasonPhrase, IHttpContent content)
+		{
+			IsSuccessStatusCode = isSuccessStatusCode;
+			StatusCode = statusCode;
+			ReasonPhrase = reasonPhrase;
+			Content = content;
+		}
+
 		public bool IsSuccessStatusCode { private set; get; }
 		public HttpStatusCode StatusCode { private set; get; }
 		public string ReasonPhrase { private set; get; }
