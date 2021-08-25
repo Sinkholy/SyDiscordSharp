24a1efbd68253f6926d3b7d5f440844d6e60d1bb
Sinkhole <Sinkholy@gmail.com>
2021-08-25 20:00:41 +0300

    [Development] SystemNetHttpClient wrapper for HttpCommunication project declaration. Pseudocode-level implementation of HttpClient wrapper for HttpCommunication project interfaces;

---
+++ SystemNetHttpCommunicationWrapper/SystemNetHttpClientWrapper.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using Http.Connection;
+
+using HttpCommunication.Connection;
+
+using HttpResponseMessage = HttpCommunication.Connection.HttpResponseMessage;
+
+namespace SystemNetHttpCommunicationWrapper
+{
+	class SystemNetHttpClientWrapper : IHttpConnection
+	{
+		const string PatchHttpMethod = "PATCH";
+		readonly HttpClient httpClient;
+
+		public SystemNetHttpClientWrapper()
+		{
+			// Инициализировать HttpClient
+			// Инициализировать Timeout стандартным значением
+			// Инициализировать BaseAddress стандартным значением
+			// Инициализировать Headers используя HeadersWrapper класс
+		}
+
+		public Uri BaseAdress { get; set; }
+		public TimeSpan Timeout { get; set; }
+		public IHeaders Headers { get; private set; }
+
+		public async Task<HttpResponseMessage> DeleteAsync(HttpRequestMessage message)
+		{
+			// Отправить запрос и дождаться его выполнения
+			// Преобразовать полученный результат в HttpResponseMessage
+			// Вернуть результат
+		}
+		public async Task<HttpResponseMessage> GetAsync(HttpRequestMessage message)
+		{
+			// Отправить запрос и дождаться его выполнения
+			// Преобразовать полученное сообщения под IHttpResponseMessage интерфейс
+			// Вернуть результат
+		}
+		public async Task<HttpResponseMessage> PatchAsync(HttpRequestMessage message)
+		{
+			// Подготовить HttpMethod в связи с тем, что Patch не входит в стандартный набор
+			// Создать System.Net.Http.HttpRequestMessage
+			// Отправить запрос и дождаться его выполнения
+			// Преобразовать полученный результат в HttpResponseMessage
+			// Вернуть результат
+		}
+		public async Task<HttpResponseMessage> PostAsync(HttpRequestMessage message)
+		{
+			// Преобразовать входной контент в HttpContent
+			// Заполнить поле Uri взяв его из параметра Message
+			// Отправить запрос и дождаться его выполнения
+			// Преобразовать полученный результат в HttpResponseMessage
+			// Вернуть результат
+		}
+		public async Task<HttpResponseMessage> PutAsync(HttpRequestMessage message)
+		{
+			// Преобразовать входной контент в HttpContent
+			// Отправить запрос и дождаться его выполнения
+			// Преобразовать полученный результат в HttpResponseMessage
+			// Вернуть результат
+		}
+		public Header GetAuthorizationHeader()
+		{
+			// Перенаправить на вызов заголовка у хттп клиента
+		}
+		public void SetAuthorizationHeader()
+		{
+			// Перенаправить на вызов заголовка у хттп клиента
+		}
+
+		HttpContent ConvertSendingContent(IHttpContent toConvert)
+		{
+			// Прочитать контент из toConvert в строчном формате
+			// Создать экземпляр типа StringContent 
+			// Подставить в StringContent контент из toConvert
+			// Вернуть результат
+		}
+		HttpResponseMessage ConvertReceivingMessage(HttpResponseMessage toConvert)
+		{
+			// Если в сообщении присутствует контент
+				// Преобразовать полученный контент в IHttpContent (ConvertReceivingContent)
+			// Преобразовать System.Net.HttpStatusCode в HttpStatusCode (ConvertSystemNetHttpStatusCode)
+			// Создать HttpResponseMessage
+			// Вернуть результат
+
+			IHttpContent ConvertReceivingContent(HttpContent toConvert)
+			{
+				// Преобразовать
+				// Вернуть результат
+			}
+			HttpStatusCode ConvertSystemNetHttpStatusCode(System.Net.HttpStatusCode toConvert)
+			{
+				// Проеобразовать
+				// Вернуть результат
+			}
+		}
+
+		class HeadersWrapper : IHeaders
+		{
+			readonly SystemNetHttpClientWrapper client;
+
+			// Конструктор
+			public Header AuthorizationHeader
+			{
+				get
+				{
+					// Перенаправить на вызов GetAuthorizeHeader у хттп клиента
+				}
+				set
+				{
+					// Перенаправить на вызов SetAuthorizeHeader у хттп клиента
+				}
+			}
+			public Header UserAgentHeader
+			{
+				get
+				{
+					// Перенаправить на вызов GetUserAgent у хттп клиента
+				}
+				set
+				{
+					// Перенаправить на вызов SetUserAgent у хттп клиента
+				}
+			}
+		}
+	}
+}
---
+++ SystemNetHttpCommunicationWrapper/SystemNetHttpMessageContentWrapper.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using HttpCommunication.Connection;
+
+namespace SystemNetHttpCommunicationWrapper
+{
+	partial class SystemNetHttpMessageContentWrapper 
+	{
+		readonly HttpContent httpContent;
+
+		SystemNetHttpMessageContentWrapper(HttpContent content)
+		{
+			httpContent = content;
+		}
+
+		internal static SystemNetHttpMessageContentWrapper WrapFrom(HttpContent content)
+		{
+			return new SystemNetHttpMessageContentWrapper(content);
+		}
+	}
+
+	partial class SystemNetHttpMessageContentWrapper : IHttpContent
+	{
+		public async Task<byte[]> ReadAsByteArrayAsync()
+		{
+			return await httpContent.ReadAsByteArrayAsync();
+		}
+
+		public async Task<Stream> ReadAsStreamAsyncAsync()
+		{
+			return await httpContent.ReadAsStreamAsync();
+		}
+
+		public async Task<string> ReadAsStringAsync()
+		{
+			return await httpContent.ReadAsStringAsync();
+		}
+	}
+}
