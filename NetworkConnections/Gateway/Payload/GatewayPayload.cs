4b3f4e5ad3ca7455f870bd4ac31ef35b4eca1537
Sinkhole <Sinkholy@gmail.com>
2021-09-06 22:18:27 +0300

    [NB][Refactor] Simplified GatewayPayload. I decided to simplify the GatewayPayload because it is not logical to have an object of type IGatewayDataObject in an object which is just a message. I want to delegate the deserialization to the final recipient of the message because the type of data may not and shouldnt be known to the message itself.

--- NetworkConnections/Gateway/Payload/GatewayPayload.cs
+++ NetworkConnections/Gateway/Payload/GatewayPayload.cs
@@ -1,15 +1,7 @@
-﻿using Gateway.Payload.DataObjects;
-using Gateway.Payload.DataObjects.Enums;
+﻿using Gateway.Payload.DataObjects.Enums;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Reflection.Emit;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace Gateway
 {
@@ -20,85 +12,44 @@ namespace Gateway
         [JsonProperty(propertyName: "op", Order = 2)]
         internal Opcode Opcode { get; private set; }
         [JsonProperty(propertyName: "d", Order = 3)]
-        internal IGatewayDataObject Data { get; private set; }
+        internal string Data { get; private set; }
         [JsonProperty(propertyName: "s", Order = 1)]
-        internal int? Sequence { get; set; }
+        internal int? Sequence { get; private set; }
         [JsonProperty(propertyName: "t", Order = 0)]
         internal string EventName { get; private set; }
-        internal GatewayPayload(Opcode opcode, IGatewayDataObject data, string eventName = null)
+        internal GatewayPayload(Opcode opcode, string data, int? sequence, string eventName = null)
         {
             Opcode = opcode;
             Data = data;
+            Sequence = sequence;
             EventName = eventName;
         }
 
         private class IGatewayPayloadConverter : JsonConverter
         {
-            public override bool CanConvert(Type objectType) => objectType == typeof(IGatewayDataObject);
+            public override bool CanConvert(Type objectType) => objectType == typeof(GatewayPayload);
             public override bool CanRead => true;
             public override bool CanWrite => false;
             public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
             {
                 JObject json = JObject.Load(reader);
-                string dataJson = json["d"].ToString(),
-                       opcodeJson = json["op"].ToString(),
-                       sequenceJson = json["s"].ToString(),
-                       eventNameJson = json["t"].ToString(),
-                       dataTypeFullName;
-                Opcode opcode = ParseOpcode(opcodeJson);
-                if(opcode == Opcode.Dispatch)
-                {
-                    dataTypeFullName = $"{objectType.Namespace}.Payload.DataObjects.Dispatch.{opcode}"; // TODO : откровенная писька
-                }
-                else if(opcode == Opcode.InvalidSession)
-                {
-                    IGatewayDataObject data = new InvalidSession(bool.Parse(dataJson));
-                    return new GatewayPayload(opcode, data, eventNameJson) { Sequence = 0 };
-                }
-                else
-                {
-                    dataTypeFullName = $"{objectType.Namespace}.Payload.DataObjects.{opcode}";
-                }
-                Type dataType = objectType.Assembly.GetType(dataTypeFullName);
-                IGatewayDataObject dataObject = JsonConvert.DeserializeObject(dataJson, dataType) as IGatewayDataObject;
-                int.TryParse(sequenceJson, out int sequence);
-                return new GatewayPayload(opcode, dataObject, eventNameJson) { Sequence = sequence };
+                string data = json["d"].ToString(),
+                       opcode = json["op"].ToString(),
+                       sequence = json["s"].ToString(),
+                       eventName = json["t"].ToString();
+                Opcode opcodeParsed = ParseOpcode(opcode);
+                int.TryParse(sequence, out int sequenceParsed);
+                return new GatewayPayload(opcodeParsed, data, sequenceParsed, eventName);
             }
-            private Opcode ParseOpcode(string opcodeJson)
+            private Opcode ParseOpcode(string input)
             {
-                byte opcode;
-                try
-                {
-                    opcode = byte.Parse(opcodeJson);
-                }
-                catch
-                {
-                    // TODO : интрумент логирования ("Cannot parse opcode in received payload.");
-                    return Opcode.UnknownOpcode;
-                }
-
-                byte[] enumValues = (byte[])Enum.GetValues(typeof(Opcode));
-                bool opcodeFounded = false;
-                for (byte i = 0; i < enumValues.Length; i++)
-                {
-                    byte current = enumValues[i];
-                    if (opcode == current)
-                    {
-                        opcodeFounded = true;
-                        opcode = current;
-                        break;
-                    }
-                    else
-                        continue;
-                }
-                if (opcodeFounded)
-                {
-                    return (Opcode)opcode;
-                }
-                else
-                {
-                    throw new Exception("unknown opcode"); //TODO : исключение
-                }
+                Opcode result = Opcode.UnknownOpcode;
+                bool converted = Enum.TryParse(input, out Opcode parsed);
+				if (converted)
+				{
+                    result = parsed;
+				}
+                return result;
             }
             /// <summary>
             /// Not implemented
