2c0a269bdd53ee96e61fa1cd8d8214d9050a1822
Sinkhole <Sinkholy@gmail.com>
2021-09-09 20:31:43 +0300

    [NB][Development & Refactor] Updated Emojies. I decided to store an string[] instead of Role[] because because I receive string[] in the message from the Discord, also added ctor and removed unused usings.

--- DiscordDataObjects/Emojis/Emoji.cs
+++ DiscordDataObjects/Emojis/Emoji.cs
@@ -1,26 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using DiscordDataObjects.Guilds;
-using DiscordDataObjects.Users;
+﻿using DiscordDataObjects.Users;
 
 namespace DiscordDataObjects.Emojis
 {
     public class Emoji : IEmoji
     {
+		const string UrlEncodedSplitter = "%3A";
+
+		public Emoji(string identifier, 
+					 string name, 
+					 bool? animated, 
+					 string[] availableForRoles, 
+					 IUser createdBy, 
+					 bool? requireColons, 
+					 bool? managed, 
+					 bool? available)
+		{
+			Identifier = identifier;
+			Name = name;
+			Animated = animated;
+			AvailableForRoles = availableForRoles;
+			CreatedBy = createdBy;
+			RequireColons = requireColons;
+			Managed = managed;
+			Available = available;
+		}
+
 		public virtual string Identifier { get; private set; }
         public string Name { get; private set; }
-        public bool IsAnimated { get; private set; }
-        public string Mention => $"<{(IsAnimated ? "a" : string.Empty)}:{Name}:{Identifier}>";
+        public bool? Animated { get; private set; }
+        public string Mention => $"<{(Animated ?? false ? "a" : string.Empty)}:{Name}:{Identifier}>";
         public bool IsUnicodeEmoji => true;
-        public string UrlEncoded => $"{Name}%3A{Identifier}";
-        public Role[] AvailableForRoles { get; private set; }
+        public string UrlEncoded => $"{Name}{UrlEncodedSplitter}{Identifier}";
+        public string[] AvailableForRoles { get; private set; }
         public IUser CreatedBy { get; private set; }
-		public bool RequireColons { get; private set; }
-		public bool Managed { get; private set; }
-		public bool Available { get; private set; }
+		public bool? RequireColons { get; private set; }
+		public bool? Managed { get; private set; }
+		public bool? Available { get; private set; }
 	}
 }
--- DiscordDataObjects/Emojis/IEmoji.cs
+++ DiscordDataObjects/Emojis/IEmoji.cs
@@ -1,7 +1,4 @@
-﻿using DiscordDataObjects.Guilds;
-using DiscordDataObjects.Users;
-
-using System;
+﻿using DiscordDataObjects.Users;
 
 namespace DiscordDataObjects.Emojis
 {
@@ -9,12 +6,12 @@ namespace DiscordDataObjects.Emojis
     {
         string Identifier { get; }
         string Name { get; }
-        Role[] AvailableForRoles { get; }
+        string[] AvailableForRoles { get; }
         IUser CreatedBy { get; }
-        bool RequireColons { get; }
-        bool Managed { get; }
-        bool IsAnimated { get; }
-        bool Available { get; }
+        bool? RequireColons { get; }
+        bool? Managed { get; }
+        bool? Animated { get; }
+        bool? Available { get; }
         bool IsUnicodeEmoji { get; }
         string Mention { get; }
         string UrlEncoded { get; }
